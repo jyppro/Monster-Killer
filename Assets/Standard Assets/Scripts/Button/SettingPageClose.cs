9e142c5369cda0dfbb58664e6949b53f07061757
LIY-Cat <43835398+LIY-Cat@users.noreply.github.com>
2024-05-29 03:18:37 +0900

    CRLF, LF 파일 문제
    해결방안 .gitattributes 추가
    그외외 .gitignore 수정 추가

--- Assets/Standard Assets/Scripts/Button/SettingPageClose.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-public class SettingPageClose : MonoBehaviour
-{
-    public GameObject SettingsPage;
-    void Start() { GetComponent<UnityEngine.UI.Button>().onClick.AddListener(CloseSettings); }
-
-    public void CloseSettings() {SettingsPage.SetActive(false); }
-}
--- Assets/Standard Assets/Scripts/StageManager.cs
+++
@@ -1,6 +0,0 @@
-using UnityEngine;
-
-public class StageManager : MonoBehaviour
-{
-    void Update() { GameManager.Instance.GetStage(); }
-}
